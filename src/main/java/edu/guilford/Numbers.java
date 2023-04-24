0318b60d3c5aee9b7eaf8fea850e9c2d033def1d
Kushi-Jakati <jakatikc@guilford.edu>
2023-04-23 21:42:52 -0400

    secondCommit

--- src/main/java/edu/guilford/Numbers.java
+++ src/main/java/edu/guilford/Numbers.java
@@ -4,18 +4,20 @@ package edu.guilford;
  * Hello world!
  *
  */
-public class Numbers 
-{
-    public static void main( String[] args )
-    {
+public class Numbers {
+    public static void main(String[] args) {
 
-        //Rational rational = new Rational();
-        //System.out.println(rational.toString());
+        // 4 Create a driver program that tests your constructor and toString method.
+        // You now have a minimal testable program. Test it and, if necessary, debug it.
+        // Did this, it works.
+
+        // Rational rational = new Rational();
+        // System.out.println(rational.toString());
         Rational rational2 = new Rational(2, 4);
         System.out.println("Here is the og object: " + rational2.toString() + "\n");
-        //rational.invert();
+        // rational.invert();
         rational2.negate();
-        System.out.println("Here is the numerator sign flipped :" + rational2.getNumerator()); 
+        System.out.println("Here is the numerator sign flipped :" + rational2.getNumerator());
         System.out.println("Here is the double for the rational number: " + rational2.toDouble() + "\n");
         System.out.println("Here is the new rational object: " + rational2.toString());
 
